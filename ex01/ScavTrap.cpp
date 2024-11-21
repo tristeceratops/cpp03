5e3ebe88cd32690d1b8b35ff9fbae89cf776276b
tristeceratops <eric.woillard@gmail.com>
2024-11-21 17:58:55 +0100

    scavtrap color

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 21:00:48 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/21 16:38:23 by ewoillar         ###   ########.fr       */
+/*   Updated: 2024/11/21 17:55:56 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ ScavTrap::ScavTrap(): ClapTrap()
 	this->energypoint = 50;
 	this->attackdamage = 20;
 	this->_isGuard = false;
-	std::cout << "ScavTrap Default Constructor called" << std::endl;
+	std::cout << "\033[33mScavTrap Default Constructor called\033[0m" << std::endl;
 }
 
 ScavTrap::ScavTrap(std::string name): ClapTrap(name)
@@ -27,23 +27,23 @@ ScavTrap::ScavTrap(std::string name): ClapTrap(name)
 	this->energypoint = 50;
 	this->attackdamage = 20;
 	this->_isGuard = false;
-	std::cout << "ScavTrap Constructor for the name " << this->_name << " called" << std::endl;
+	std::cout << "\033[33mScavTrap Constructor for the name " << this->_name << " called\033[0m" << std::endl;
 }
 
 ScavTrap::ScavTrap(const ScavTrap &copy): ClapTrap(copy)
 {
 	*this = copy;
-	std::cout << "ScavTrap Copy Constructor called" << std::endl;
+	std::cout << "\033[33mScavTrap Copy Constructor called\033[0m" << std::endl;
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << "ScavTrap Deconstructor for " << this->_name << " called" << std::endl;
+	std::cout << "\033[33mScavTrap Deconstructor for " << this->_name << " called\033[0m" << std::endl;
 }
 
 ScavTrap &ScavTrap::operator=(const ScavTrap &src)
 {
-	std::cout << "ScavTrap Assignation operator called" << std::endl;
+	std::cout << "\033[33mScavTrap Assignation operator called\033[0m" << std::endl;
 	this->_name = src._name;
 	this->healpoint = src.healpoint;
 	this->energypoint = src.energypoint;
@@ -55,11 +55,11 @@ void ScavTrap::attack(const std::string& target)
 {
 	if (energypoint > 0 && healpoint > 0)
 	{
-		std::cout << "Scavtrap " << _name << " attacks " << target << " ,causing " << this->attackdamage << " points of damage !" << std::endl;
+		std::cout << "\033[33mScavtrap " << _name << " attacks " << target << " ,causing " << this->attackdamage << " points of damage !\033[0m" << std::endl;
 		this->energypoint--;
 	}
 	else
-		std::cout << "Scavtrap " <<_name <<  (energypoint > 0 ? " is already dead" :" is too low on energy !" ) << std::endl;
+		std::cout << "\033[33mScavtrap " <<_name <<  (energypoint > 0 ? " is already dead" :" is too low on energy !\033[0m" ) << std::endl;
 }
 
 void	ScavTrap::guardGate(void)
@@ -67,7 +67,7 @@ void	ScavTrap::guardGate(void)
 	if (this->_isGuard == false)
 	{
 		this->_isGuard = true;
-		std::cout << "ScavTrap " << this->_name << " is now guarding the gate." << std::endl;
+		std::cout << "\033[33mScavTrap " << this->_name << " is now guarding the gate.\033[0m" << std::endl;
 	}
 	else
 		std::cout << "\033[33mScavTrap " << this->_name << " is already guarding the gate.\033[0m" << std::endl;
